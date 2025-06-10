87f38057f06e9b65811e4cb7202d395a7006552e
Mar.io <34627453+Maar-io@users.noreply.github.com>
2025-06-10 17:20:46 +0200

    added debug event

--- src/processor.ts
+++ src/processor.ts
@@ -2,8 +2,8 @@ import { Counter } from '@sentio/sdk'
 import { EthChainId } from '@sentio/sdk/eth'
 import { ClaimAcsProcessor } from './types/eth/claimacs.js'; // Path to generated file
 
-const claimAmountValue = Counter.register('claim_amount_value');
-const claimCounter = Counter.register('claim_counter');
+const claimAmountValue = Counter.register("claim_amount_value", { description: "Claimed Amount", unit: "ASTR" })
+const claimCounter = Counter.register("claim_counter", { description: "Claim counter", unit: "claims" })
 
 const CLAIM_CONTRACT = '0x01225c7bDF26ea76318bB717be5A99906cE713Bf'
 const NETWORK = EthChainId.SONEIUM_MAINNET
@@ -15,6 +15,14 @@ ClaimAcsProcessor.bind({
 
 }).onEventClaimed(async (event, ctx) => {
   const val = event.args.amount.scaleDown(18) // Scale down the value from 18 decimals to 0
-  claimAmountValue.add(ctx, val)
-  claimCounter.add(ctx, 1)
+  claimAmountValue.add(ctx, val, { token: "value" })
+  claimCounter.add(ctx, 1, { token: "claims" })
+  
+  // Optionally, log the event details (useful for debugging)
+  ctx.eventLogger.emit('ClaimProcessed', {
+    distinctId: event.args.account, // Associate log with recipient
+    amount: event.args.amount.toString(), // Log the scaled value
+    to: event.args.account, // Log the recipient address
+    message: `Processed claim from ${event.args.account} for amount ${event.args.amount.toString}`
+  })
 })
